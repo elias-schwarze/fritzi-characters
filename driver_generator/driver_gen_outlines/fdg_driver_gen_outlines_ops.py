5c0c958f7ecd898e8c035fa994e16d3d83f1d5e8
MichaelSchieberBuero <MichaelDSchieber@gmx.de>
2022-08-19 15:14:32 +0200

    Second part of Outline driver Commit. Didnt get added the first time weirdly enough

--- driver_generator/driver_gen_outlines/fdg_driver_gen_outlines_ops.py
+++ driver_generator/driver_gen_outlines/fdg_driver_gen_outlines_ops.py
@@ -40,12 +40,15 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
         pass_name = wm.Pass_Name
         cam_empty = bpy.context.scene.objects.get(fdg_names.empty_cam)
 
+
+        # This adds the new outline Pass to a Settings Propertygroup stored in a propertycollection in the scene
         item = context.scene.gp_settings.add()
         item.name=pass_name
         setting_index = context.scene.gp_settings.find(pass_name)
         
         item.gp_object=gp
         
+        # These are the default Values of the Settings
         item.thick_dist_close = 1.0
         item.thick_dist_far = 2.0
         item.thick_close = 1.0
@@ -60,6 +63,8 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
 
         modifier_name = "pass_" + str(pass_nr) + "_" + pass_name + "_Thickness"
         
+        # This is not used yet. When implemented the user can choose to create a new GP and View layer (or choose existing ones) which get automatically set up
+        # to work with the Render pass System from Christoph and Elias. Needs to be implemented in the UI and might also need to setup a lineart Modifier.
         if wm.gp_auto_advanced_toggle:
 
             view_layer : string
@@ -82,7 +87,10 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
 
             item.GP_layer = gp_layer.info
 
-        print(modifier_name)
+        
+        # A Cam Empty will be created (if there is not already one) and parented to the active Camera.
+        # The auto Camera Parenting option might be wanted to be activated, so the distance is always 
+        # calculated from the current active Camera.
 
         if cam_empty is None:
             cam_empty = bpy.data.objects.new(fdg_names.empty_cam, None)
@@ -99,18 +107,9 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
 
         cam_empty.hide_viewport = True
 
-        add_custom_property(gp, "Close Distance", default=8.0, prop_min=-1000.0, prop_max=1000.0)
-        add_custom_property(gp, "Far Distance", default=23.5, prop_min=-1000.0, prop_max=1000.0)
-        add_custom_property(gp, "Close Thickness", default=24.0, prop_min=0.0, prop_max=1000.0, description="The thickness multiplier applied at the minimum (close) distance")
-        add_custom_property(gp, "Far Thickness", default=30.0, prop_min=0.0, prop_max=1000.0, description="The thickness multiplier applied at the maximum (far) distance.")
-
-        add_custom_property(gp, "Curve Start Distance", default=8.0, prop_min=-1000.0, prop_max=1000.0, description="The Distance at which the Custom Curve is fully applied")
-        add_custom_property(gp, "Curve End Distance", default=10.0, prop_min=-1000.0, prop_max=1000.0, description="The Distance at which the Custom Curve is completely off")
-        
-        gp["Grease Pencil Name"] = gp.name
-
-        
-
+        # This creates a Thickness Modifier which has a custom Curve attached to it. It can interpolate between
+        # the curve affecting the Line or the curve not affecting the line. It takes an amount which will make the
+        # dynamic line more prominent (or less prominent)
         curve_modifier = gp.grease_pencil_modifiers.new(modifier_name + "_CURVE", 'GP_THICK')
         item.crv_modifier = modifier_name + "_CURVE"
 
@@ -122,6 +121,7 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
         curve_modifier.curve.curves[0].points.new(1.0, 0.0)
         curve_modifier.curve.update()
 
+        # The Values which get used here are stored in the FritziGPSetup PropertyGroup in a Collectionproperty in the scene
         data_path_start = '["gp_settings"][' + str(setting_index) + ']'
         add_var(curve_driver, context.scene, "crv_max_dist", type='SINGLE_PROP', rna_data_path=data_path_start + '["crv_max_dist"]', id_type='SCENE')
         add_var(curve_driver, context.scene, "crv_off_dist", type='SINGLE_PROP', rna_data_path=data_path_start + '["crv_off_dist"]', id_type='SCENE')
@@ -138,8 +138,16 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
         target.id = wm.character_rig
         target.bone_target = wm.character_rig_bone
 
+        # This expression calculates the curve amount based on the distance from the camera(empty) to the Character Rig (Head Bone).
+        # It uses Linear interpolation between 1 (no Curve) and (1+crv_amount) depending on the distance Values set by the user.
+        # The Thickness modifier shows a Curve if the thickness_factor is greater than 1 (2 being the standard Curve). This is weird, but it works.
+        # It also scales the curve, so it has to be counterscaled accordingly in the main thickness modifier.
+        # If the User chooses the Dynamic Line to be always the same the off distance gets set to -1 and the max distance gets set to 0.
+        # Since the Distance to the Camera is always positive this expression then always returns the max Amount.
         curve_driver.expression = "max(1, min(1 + crv_amount, ((1 * (1 - (dist - crv_off_dist) / (crv_max_dist - crv_off_dist))) + ((1 + crv_amount) * (dist - crv_off_dist) / (crv_max_dist - crv_off_dist)) ) ))"
 
+        # This creates the Main Thickness modifier which scales the thickness based on the distance from the Camera(empty) to the Character Rig
+        # If the Camera gets closer to the Character the thickness_factor gets lower, so the outline doesnt get too thick.
         thickness_modifier = gp.grease_pencil_modifiers.new(modifier_name + "_MAIN", 'GP_THICK')
         item.thick_modifier = modifier_name + "_MAIN"
         thickness_modifier.layer_pass = pass_nr
@@ -152,11 +160,6 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
         add_var(thickness_driver, context.scene, "far_thick", type='SINGLE_PROP', rna_data_path=data_path_start + '["thick_far"]', id_type='SCENE')
         add_var(thickness_driver, gp, "crv_value", type='SINGLE_PROP', rna_data_path='grease_pencil_modifiers["'+ modifier_name + '_CURVE"].thickness_factor')
 
-        #var = thickness_driver.variables.new()
-        #var.name = "crv_value"
-        #var.type = 'SINGLE_PROP'
-
-
         var = thickness_driver.variables.new()
         var.name = "dist"
         var.type = 'LOC_DIFF'
@@ -168,6 +171,10 @@ class FDG_OT_GenerateOutlineDriver_Op(Operator):
         target.id = wm.character_rig
         target.bone_target = wm.character_rig_bone
 
+        # This Expression scales the Thickness modifier based on the Distance from Cam to Character and the distance and thickness values given
+        # by the user. It also defaults to clamping the maximum thickness, because that is the default wanted behaviour.
+        # Clamping modes can be changed from the user in the Settings which will update the Expression in the Driver.
+        # The calculated Value will be divided by the thickness from the Curve Thickness Modifier to counterscale the thickness which is used to activate the dynamic Line.
         thickness_driver.expression = "min(far_thick, ((close_thick * (1 - (dist - close_dist) / (far_dist - close_dist))) + (far_thick * (dist - close_dist) / (far_dist - close_dist))))/crv_value"
 
 
