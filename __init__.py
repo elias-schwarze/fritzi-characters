828deae35be97170b543a6d46bed802cf273fb1e
MichaelSchieberBuero <MichaelDSchieber@gmx.de>
2023-01-12 15:48:04 +0100

    v1.6.26

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ bl_info = {
     "author": "Michael Schieber, Sophie Fink, Elias Schwarze",
     "description": "A suite of tools for character rigging in production",
     "blender": (3, 0, 1),
-    "version": (1, 6, 25),
+    "version": (1, 6, 26),
     "location": "3D Viewport > Properties panel (N) > FCHAR Tabs",
     "category": "Rigging"
 }
