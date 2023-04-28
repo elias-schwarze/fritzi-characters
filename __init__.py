824e3fd6459f0659ad66d8839c59649e9789ac47
MichaelSchieberBuero <MichaelDSchieber@gmx.de>
2023-04-28 13:00:40 +0200

    v1.7.0

--- __init__.py
+++ __init__.py
@@ -33,8 +33,8 @@ bl_info = {
     "name": "fritziCharacters",
     "author": "Michael Schieber, Sophie Fink, Elias Schwarze",
     "description": "A suite of tools for character rigging in production",
-    "blender": (3, 0, 1),
-    "version": (1, 6, 42),
+    "blender": (3, 3, 0),
+    "version": (1, 7, 0),
     "location": "3D Viewport > Properties panel (N) > FCHAR Tabs",
     "category": "Rigging"
 }
